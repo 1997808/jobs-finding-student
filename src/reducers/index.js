ce48a740aded853ba44ce8363a1138766803a01a
1997808 <khanhhuyle22@gmail.com>
2020-06-24 22:46:50 +0700

    setup folder

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+// reducers/index.js
+
+// import { combineReducers } from 'redux'
+// import people from './peopleReducer'
+// import cartReducer from './cartReducer'
+// định nghĩa các reducer khác ...
+
+// const rootReducer = combineReducers({
+//   people,
+//   cartReducer
+//   // nếu có reducer khác thì add thêm ở đây
+// })
+
+// export default rootReducer
