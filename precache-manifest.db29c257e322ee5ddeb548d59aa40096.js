cacc18c66713f21bfcc338562920e23f9eb67263
1997808 <khanhhuyle22@gmail.com>
2020-06-29 12:50:09 +0700

    Updates

---
+++ precache-manifest.db29c257e322ee5ddeb548d59aa40096.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5d44e23256754819159990a8f0d561",
+    "url": "/jobs-finding-student/index.html"
+  },
+  {
+    "revision": "43111d95a1899959a9d5",
+    "url": "/jobs-finding-student/static/css/main.a512f0d9.chunk.css"
+  },
+  {
+    "revision": "1f4625bae3e0a8894703",
+    "url": "/jobs-finding-student/static/js/2.e6538581.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jobs-finding-student/static/js/2.e6538581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43111d95a1899959a9d5",
+    "url": "/jobs-finding-student/static/js/main.63ce8bf2.chunk.js"
+  },
+  {
+    "revision": "62393e1455f2f31b344f",
+    "url": "/jobs-finding-student/static/js/runtime-main.90d0513c.js"
+  },
+  {
+    "revision": "ad3b151202adcc82aa1ca2dafa760a4f",
+    "url": "/jobs-finding-student/static/media/1_qdFdhbR00beEaIKDI_WDCw.ad3b1512.gif"
+  },
+  {
+    "revision": "d0520ddaddaa030a86b7df38b2db57df",
+    "url": "/jobs-finding-student/static/media/715272_l.d0520dda.jpg"
+  },
+  {
+    "revision": "493c865acbe8f61bea6a9c23b916c6d8",
+    "url": "/jobs-finding-student/static/media/IMG_20190317_171216.493c865a.jpg"
+  },
+  {
+    "revision": "21a5e58cb9c7a3359950a4e3f0a9ab70",
+    "url": "/jobs-finding-student/static/media/Screenshot from 2020-06-29 11-08-24.21a5e58c.png"
+  },
+  {
+    "revision": "485ea4590b180bd9b4a2c3fe7081f6c1",
+    "url": "/jobs-finding-student/static/media/Screenshot from 2020-06-29 11-10-34.485ea459.png"
+  },
+  {
+    "revision": "70b2c0813a125da466b194ab4ccc4769",
+    "url": "/jobs-finding-student/static/media/avatars-000364906919-fdvn9m-t500x500.70b2c081.jpg"
+  },
+  {
+    "revision": "182993435083d8c750c30602da696ea7",
+    "url": "/jobs-finding-student/static/media/small.18299343.jpg"
+  }
+]);
